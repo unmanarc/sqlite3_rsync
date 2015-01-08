0411266547c82a595f51c0d2c06aa1caa4af372e
Aaron Mizrachi <aaron@unmanarc.com>
2015-01-08 10:49:32 -0500

    fix on verbosity

--- src/verbose.h
+++ src/verbose.h
@@ -14,7 +14,7 @@ extern globalArgs_t globalArgs;
 #define CONNECTION_FAILURE_RET(x,y) x.Close();if(globalArgs.verbosity>0) fprintf(stderr,"[%p] %s\n",this,y); fflush(stderr); return
 #define PRINT_ON_VERBOSE(x) if(globalArgs.verbosity>0) { fprintf(stdout,"[%p] %s\n",this,x);  fflush(stdout);} globalArgs.verbosity+1
 #define PRINT_ON_VERBOSE_2(x,y) if(globalArgs.verbosity>0) { fprintf(stdout,"[%p] %s : %s\n",this,x,y);  fflush(stdout);} globalArgs.verbosity+1
-#define PRINT_ON_VERBOSE_STRING_LIST(x,y) if(globalArgs.verbosity>0) { printf("[%p] %s: ",this,x);bool f2a=false;for (std::string oid : y){ if (f2a) { f2a = true; printf("%s",oid.c_str()); } else printf(",%s",oid.c_str());} printf("\n"); fflush(stdout);} globalArgs.verbosity+1
+#define PRINT_ON_VERBOSE_STRING_LIST(x,y) if(globalArgs.verbosity>0) { printf("[%p] %s: ",this,x);bool f2a=true;for (std::string oid : y){ if (f2a) { f2a = false; printf("%s",oid.c_str()); } else printf(",%s",oid.c_str());} printf("\n"); fflush(stdout);} globalArgs.verbosity+1
 
 
 #endif /* VERBOSE_H_ */
